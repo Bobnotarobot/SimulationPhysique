04964982cef324f701d9be7b9ea0f31993f03fb8
Elliot <49864051+Elliot-Jullier@users.noreply.github.com>
2021-03-05 18:54:11 +0000

    Started Adding Angular Acceleration for prograde orientation

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -11,17 +11,19 @@ public class Rocket : MonoBehaviour
     private float lunarModuleDryMass = 4280; // kg
     private float lunarModuleFuelMass = 10920; // kg
     private float orbitingStageDryMass = 23572; // kg
-    
+
     public Moon moon;
     public Constants constants;
 
     private Vector3 position;
     private Vector3 velocity;
     private Vector3 acceleration;
-    
+    private float accelerationMulitplier; //Engine acceleration depends on mass left in rocket
+
     private Vector3 orientation;
     private Vector3 angularVelocity;
     private Vector3 angularAcceleration;
+    private float angularAccelerationMultiplier; //RCS Boosters acceleration depends on mass left in rocket
 
 
     void Start()
@@ -44,13 +46,13 @@ public class Rocket : MonoBehaviour
 
         transform.position = position / constants.scale;
 
-        angularAcceleration = GetAngularAcceleration();
+        angularAcceleration = getAngularAcceleration(velocity, orientation, angularVelocity);
         angularVelocity += angularAcceleration * constants.fixedUpdateMultiplier * constants.timeMultiplier;
         orientation += angularVelocity * constants.fixedUpdateMultiplier * constants.timeMultiplier;
-       
+
         transform.Rotate(orientation / constants.scale);
     }
-    
+
     float GetDistance(Vector3 pos1, Vector3 pos2)
     {
         float distance = 0;
@@ -66,9 +68,43 @@ public class Rocket : MonoBehaviour
         return acceleration;
     }
 
-    Vector3 GetAngularAcceleration()
+    Vector3 angularAccelerationDirection()
     {
-        Vector3 placeholder = new Vector3(0, 0, 0);
-        return placeholder;
+        Vector3 angleRotatedUntilStop = constants.nullVector;
+        Vector3 rotationTillVelocity = constants.nullVector;
+        Vector3 angularAccelerationDirection = constants.nullVector;
+        for (int i = 0; i < 3; i++)
+        {
+            angleRotatedUntilStop[i] = -1 * Mathf.Pow(angularVelocity[i], 2) / (2 * angularAcceleration[i]);
+            rotationTillVelocity[i] = orientationDifference[i] - angleRotatedUntilStop[i];
+            if (rotationTillVelocity[i] < 0) { angularAccelerationDirection[i] = -1; }
+            else if (rotationTillVelocity[i] > 0) { angularAccelerationDirection[i] = 1; }
+        }
+        return angularAccelerationDirection;
+    }
+
+    Vector3 getAngularAcceleration(Vector3 targetOrientation)
+    {
+        Vector3 angularAcceleration = constants.nullVector;
+        Vector3 orientationDifference; //How do we fkn calculate this?!
+        for (int i = 0; i < 3; i++)
+        {
+            if (orientationDifference[i] == 0)
+            {
+                if (angularVelocity[i] == 0)
+                {
+                    angularAcceleration[i] = 0;
+                }
+                else if (angularVelocity[i] > 0)
+                {
+                    angularAcceleration[i] = -1 * angularAccelerationMultiplier;
+                }
+                else
+                {
+                    angularAcceleration[i] = angularAccelerationMultiplier;
+                }
+
+            }
+        }
     }
 }
