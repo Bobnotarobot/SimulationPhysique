b44b0a63f7aa2e769f266080ba9224d4a0fc52f3
Elliot <49864051+Elliot-Jullier@users.noreply.github.com>
2021-03-07 00:39:11 +0000

    Added time UI to my scene, reworked angular acceleration
    The time isn't displayed in one line but on 2. The calculation to do the angles doesn't work (or the angular acceleration calculation), the acceleration system does though

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -35,18 +35,19 @@ public class Rocket : MonoBehaviour
         orientation = new Vector3(0, 0, 0);
         transform.Rotate(orientation / constants.scale); // use 'tranform.Rotate(x);' not 'transform.rotation = x;' because using 3 angles not quaternions 
         angularVelocity = new Vector3(0, 0, 0);
+
     }
 
     void FixedUpdate()
     {
-        acceleration = GetAcceleration(position, moon.position);
-        // Debug.Log(acceleration);
+        acceleration = GetGravityAcceleration(position, moon.position);
         velocity += acceleration * constants.fixedUpdateMultiplier * constants.timeMultiplier;
         position += velocity * constants.fixedUpdateMultiplier * constants.timeMultiplier;
 
         transform.position = position / constants.scale;
 
-        //angularAcceleration = getAngularAcceleration(velocity.normalized); 
+        angularAccelerationMultiplier = 1960 / lunarModuleFuelMass; // 4 thursters of 490N each
+        angularAcceleration = GetAngularAcceleration(velocity, angularVelocity, angularAccelerationMultiplier); 
         angularVelocity += angularAcceleration * constants.fixedUpdateMultiplier * constants.timeMultiplier;
         orientation += angularVelocity * constants.fixedUpdateMultiplier * constants.timeMultiplier;
 
@@ -61,96 +62,102 @@ public class Rocket : MonoBehaviour
         return distance;
     }
 
-    Vector3 GetAcceleration(Vector3 posRocket, Vector3 posMoon) // Assuming the only force exerted on the rocket is the gravity of the moon
+    float Magnitude(Vector3 vector)
+    {
+        return (Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2) + Mathf.Pow(vector.z, 2)));
+    }
+    
+    Vector3 GetGravityAcceleration(Vector3 posRocket, Vector3 posMoon) // Assuming the only force exerted on the rocket is the gravity of the moon
     {
         Vector3 acceleration = posMoon - posRocket;
         acceleration = acceleration.normalized * constants.gravConst * moon.moonMass / Mathf.Pow(GetDistance(posRocket, posMoon), 2);
         return acceleration;
     }
 
-    Vector3 orientationVector(Vector3 orientation)
+    Vector3 AngleToRotate(Vector3 orientationAngles, Vector3 targetVector)
     {
-        Vector3 orientationVector = new Vector3(-1 * Mathf.Sin(orientation[1]) * Mathf.Sin(orientation[2]), Mathf.Cos(orientation[0]) * Mathf.Cos(orientation[2]), Mathf.Cos(orientation[0]) * Mathf.Cos(orientation[1]));
-        // Calculating the vector based on my hypotheses that the x value is not affected when rotated around the x axis, 
-        //and that the vector depends on the multiplication of whatever method is used to calculate it individually, 
-        //and those were teseted individually (explaines the sin(-z) for the x value)
-        return orientationVector;
-    }
-
-    Vector3 angleToRotate(Vector3 orientationVector, Vector3 targetVector)
-    {
-        Vector3 orientationAngles = constants.nullVector;
         Vector3 targetAngles = constants.nullVector;
-        Vector3 angleDifference = constants.nullVector;
+        Vector3 angleToRotate = constants.nullVector;
         
-        for (int i = 0; i < 3; i++) // i = 0 - x-angle, i = 1 - y-angle, i = 3 - z-angle
-        { 
-            if (orientationVector[(i+1)%3] == 0) // can't use arctan if the bottom value is 0 ie. it is vertical
+        // Calculate the global orientation of the target vector
+        float targetMagnitude = Magnitude(targetVector);
+        for (int i = 0; i < 3; i++)
+        {
+            targetAngles[i] = Mathf.Acos(targetVector[i] / targetMagnitude);
+        }
+        
+        targetAngles *= 180 / Mathf.PI;
+        angleToRotate = targetAngles - orientationAngles;
+        for (int i = 0; i < 3; i++)
+        {
+            if (angleToRotate[i] > 180)
             {
-                orientationAngles[i] = 90 * orientationVector[(i+1)%3] / Mathf.Abs(orientationVector[(i+1)%3]);
+                angleToRotate[i] -= 360;
             }
-            else
+            else if (angleToRotate[i] < -180)
             {
-                orientationAngles[i] = Mathf.Atan(orientationVector[(i+2)%3] / orientationVector[(i+1)%3]);
-            }
-            // angles for target
-            if (targetVector[(i+1)%3] == 0)
-            {
-                targetAngles[i] = 90 * targetVector[(i+1)%3] / Mathf.Abs(targetVector[(i+1)%3]);
-            }
-            else
-            {
-                targetAngles[i] = Mathf.Atan(targetVector[(i+2)%3] / targetVector[(i+1)%3]);
-            }
-            // angles difference
-            angleDifference[i] = Mathf.Abs(targetAngles[i] - orientationAngles[i]);
-            if (angleDifference[i] > 180) // to have an angle under 180 degres --> be efficient when rotating
-            {
-                angleDifference[i] = 360 - angleDifference[i];
+                angleToRotate[i] += 360;
             }
         }
-        return angleDifference;
+
+        return angleToRotate;
     }
+
+    
+    float AccelerationDirection( float angleToRotate, float angularVelocity, float angularAcceleration)
+    {
+        float angleRotatedAtStop = -1 * Mathf.Pow(angularVelocity, 2) / (2 * angularAcceleration);
+        float angleToGo = angleToRotate - angleRotatedAtStop;
+        if (angleToGo > 0 && angleToRotate > 0 || angleToGo >= 0 && angleToRotate < 0)
+        {
+            return 1;
+        }
+        return 0;
+    }
+        
+    Vector3 GetAngularAcceleration(Vector3 targetVector, Vector3 angularVelocity, float angularAccelerationMultiplier)
+    {
+        Vector3 accelerationVector = constants.nullVector;
+        Vector3 angleToRotate = AngleToRotate(orientation, targetVector);
+        for (int i = 0; i < 3; i++) 
+        {
+            if (angleToRotate[i] == 0 && angularVelocity[i] == 0)
+            {
+                accelerationVector[i] = 0;
+            }
+            else if (angleToRotate[i] == 0 && angularVelocity[i] < 0)
+            {
+                accelerationVector[i] = 1;
+            }
+            else if (angleToRotate[i] == 0 && angularVelocity[i] > 0)
+            {
+                accelerationVector[i] = -1;
+            }
+            else if (angleToRotate[i] < 0 && angularVelocity[i] == 0)
+            {
+                accelerationVector[i] = -1;
+            }
+            else if (angleToRotate[i] > 0 && angularVelocity[i] == 0)
+            {
+                accelerationVector[i] = 1;
+            }
+            else if (angleToRotate[i] < 0 && angularVelocity[i] < 0)
+            {
+                accelerationVector[i] = AccelerationDirection(angleToRotate[i], angularVelocity[i], angularAccelerationMultiplier);
+            }
+            else if (angleToRotate[i] > 0 && angularVelocity[i] > 0)
+            {
+                accelerationVector[i] = AccelerationDirection(angleToRotate[i], angularVelocity[i], -angularAccelerationMultiplier);
+            }
+        }
+        accelerationVector *= angularAccelerationMultiplier;
+        return accelerationVector;
+    }
+
+
+
 
 }
     
-    /*
-    Vector3 angularAccelerationDirection()
-    {
-        Vector3 angleRotatedUntilStop = constants.nullVector;
-        Vector3 rotationTillVelocity = constants.nullVector;
-        Vector3 angularAccelerationDirection = constants.nullVector;
-        for (int i = 0; i < 3; i++)
-        {
-            angleRotatedUntilStop[i] = -1 * Mathf.Pow(angularVelocity[i], 2) / (2 * angularAcceleration[i]);
-            rotationTillVelocity[i] = orientationDifference[i] - angleRotatedUntilStop[i];
-            if (rotationTillVelocity[i] < 0) { angularAccelerationDirection[i] = -1; }
-            else if (rotationTillVelocity[i] > 0) { angularAccelerationDirection[i] = 1; }
-        }
-        return angularAccelerationDirection;
-    }
 
-    Vector3 getAngularAcceleration(Vector3 targetOrientation)
-    {
-        Vector3 angularAcceleration = constants.nullVector;
-        Vector3 orientationDifference; //How do we fkn calculate this?!
-        for (int i = 0; i < 3; i++)
-        {
-            if (orientationDifference[i] == 0 && angularVelocity[i] == 0)
-            {
-                angularAcceleration[i] = 0;
-            }
-            else if (orientationDifference[i] == 0 && angularVelocity[i] < 0)
-            {
-                angularAcceleration[i] = angularAccelerationMultiplier;
-            }
-            else if (orientationDifference[i] == 0 && angularVelocity[i] > 0)
-            {
-                angularAcceleration[i] = -1 * angularAccelerationMultiplier;
-            }
-            else if (orientationDifference[i] < 0 && angularVelocity[i] = 0) { }
-
-        }
-    }
-    */
 
