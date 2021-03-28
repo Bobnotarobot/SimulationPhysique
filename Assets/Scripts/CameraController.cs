cdf4cb435487a89bcbae2b7c10fb8a520c7206f3
Bobnotarobot <raphael.tasseel@gmail.com>
2021-03-28 22:53:52 +0100

    double camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera rocketCamera;
+    public Camera mainCamera;
+    
+    void Start()
+    {
+        if (rocketCamera == null)
+        {
+            rocketCamera = GameObject.FindGameObjectWithTag("RocketCamera").GetComponent<Camera>();
+        }
+        if (mainCamera == null)
+        {
+            mainCamera = this.GetComponent<Camera>();
+        }
+
+        rocketCamera.rect = new Rect(0, 0, 0.5f, 1);
+        mainCamera.rect = new Rect(0.5f, 0, 0.5f, 1);
+    }
+
+    void Update()
+    {
+        
+    }
+}
